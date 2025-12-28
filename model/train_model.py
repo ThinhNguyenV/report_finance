a180530b38ac0e74002d6c4c2431951591f40744
ThinhNguyenV <vanthinh2231@gmail.com>
2025-12-28 14:16:35 +0700

    update

--- model/train_model.py
+++ model/train_model.py
@@ -4,11 +4,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 import joblib
 
-# Đọc dữ liệu đã tiền xử lý
+# Read preprocessed data
 df = pd.read_csv('data/model/preprocessed_finance_data.csv', index_col='Date')
 
-# Xác định features và target
-# Dự báo 'Doanh thu'
+# Target variable and features
+# Parameters for the model
 features = ['day_of_week', 'day_of_month', 'month', 'revenue_lag_1', 'revenue_lag_2', 
             'revenue_lag_3', 'revenue_lag_4', 'revenue_lag_5', 'revenue_lag_6', 
             'revenue_lag_7', 'revenue_rolling_mean_7']
@@ -17,25 +17,25 @@ target = 'Doanh thu'
 X = df[features]
 y = df[target]
 
-# Chia dữ liệu huấn luyện và kiểm tra (80/20)
+# Split data to training and testing sets (80% train, 20% test)
 split_idx = int(len(df) * 0.8)
 X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
 y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
 
-# Khởi tạo và huấn luyện mô hình
+# Train model Random Forest
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Dự đoán trên tập kiểm tra
+# Predict in test set
 y_pred = model.predict(X_test)
 
-# Đánh giá mô hình
+# Evaluate model
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
 print(f"Mean Absolute Error: {mae}")
 print(f"Root Mean Squared Error: {rmse}")
 
-# Lưu mô hình
+# Save model
 joblib.dump(model, 'model/finance_forecast_model.pkl')
 print("Save model.")
